8593aac59783c03e7b28ffd9cd3bbd896154c79a
sogand ghasempour <sogand.ghasempour@gmail.com>
2025-10-20 19:33:55 +0330

    In this stage,we load data and we build Custom pipeline

--- AggressionScorer/Program.cs
+++ AggressionScorer/Program.cs
@@ -10,18 +10,41 @@ namespace AggressionScorer
         static void Main(string[] args)
         {
             Console.WriteLine("Aggression scorer model builder started!");
-
+            var mlContext = new MLContext(0);
+            //Load data
+            // store the data in RAM and then users comments converted to floats so that they are understandable to the machine.
+            var inputDataPreparer = mlContext.Transforms.Text.FeaturizeText("Features","Comment").AppendCacheCheckpoint(mlContext);
+            //Choosing a scenario
+            var trainer = mlContext.BinaryClassification.Trainers.LbfgsLogisticRegression();
+            var trainingPipeline = inputDataPreparer.Append(trainer);
             //Load data 
 
+            var createInputFile = @"Data\preparedInput.tsv";
+            DataPreparer.CreatePreparedDataFile(createInputFile, true);
+
+            IDataView traninDataView = mlContext.Data.LoadFromTextFile<ModelInput>(
+            path: createInputFile,
+            hasHeader: true,
+            separatorChar: '\t',
+            allowQuoting: true
+                );
 
 
             //Fit the model
+            ITransformer model = trainingPipeline.Fit(traninDataView);
 
             //Test the model
 
 
             //Save the model
-         
+            if (!Directory.Exists("Model"))
+            {
+                Directory.CreateDirectory("Model");
+            }
+            var modelFile = @"Model\\AggressionScoreModel.zip";
+            mlContext.Model.Save(model, traninDataView.Schema, modelFile);
+
+            Console.WriteLine("Done !");
         }
     }
 }
